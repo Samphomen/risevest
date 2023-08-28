8bfb730b0c436496443c3ceca2e231633688673c
Samphomen <ishaqcole@gmail.com>
2023-08-28 23:11:10 +0100

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const cors = require("cors")
 const fileUpload = require("express-fileupload");
 const multer = require('multer')
